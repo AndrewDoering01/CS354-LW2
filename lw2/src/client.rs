92dd9b033959db6135cb9f369618d458b449c0e9
AndrewDoering01 <andrewdoering@u.boisestate.edu>
2023-04-24 13:01:11 -0600

    cleanup

--- lw2/src/client.rs
+++ lw2/src/client.rs
@@ -7,16 +7,14 @@ fn main() -> std::io::Result<()> {
     let mut stream = TcpStream::connect("127.0.0.1:8080")?;
 
     loop {
-        let request = "Ping".to_string();
+        let request = String::from("Ping");
         stream.write(request.as_bytes())?;
         stream.flush().unwrap();
         println!("Sent request: {}", request);
-
         let mut response_buffer = [0; 1024]; // Read up to 1024 bytes
         let num_bytes = stream.read(&mut response_buffer)?;
-        let response = String::from_utf8_lossy(&response_buffer[..num_bytes]).to_string();
-        println!("Received response: {}", response);
-
+        let response = String::from_utf8_lossy(&response_buffer[..num_bytes]);
+        println!("Received response: {response}");
         thread::sleep(Duration::from_secs(1));
     }
 }
